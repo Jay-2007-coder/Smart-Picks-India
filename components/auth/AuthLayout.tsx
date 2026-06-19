c9c54e48116196a47c5892efc52745b94d4da492
Jay <jaytalekar82@gmail.com>
2026-06-19 19:01:22 +0530

    fix(auth): correct AuthLayout sizing to prevent clipping of the card

--- components/auth/AuthLayout.tsx
+++ components/auth/AuthLayout.tsx
@@ -6,21 +6,21 @@ import { AuthHero } from "./AuthHero";
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative min-h-screen w-full flex bg-[#050816] overflow-hidden">
+    <div className="relative min-h-[calc(100vh-64px)] w-full flex bg-[#050816] overflow-hidden">
       {/* 1. Global Background */}
       <AuthBackground />
 
       {/* 2. Content Container */}
-      <div className="relative z-10 w-full max-w-7xl mx-auto flex flex-col lg:flex-row h-screen">
+      <div className="relative z-10 w-full max-w-7xl mx-auto flex flex-col lg:flex-row min-h-[calc(100vh-64px)] lg:h-[calc(100vh-64px)]">
         
         {/* Left Column: Hero (Desktop Only) */}
-        <div className="hidden lg:flex flex-col flex-1 p-12 xl:p-20 relative">
+        <div className="hidden lg:flex flex-col flex-1 p-12 xl:p-20 relative h-full">
           <AuthHero />
         </div>
 
         {/* Right Column: Auth Form */}
-        <div className="flex-1 flex flex-col items-center justify-center p-6 sm:p-12 lg:p-20 h-full overflow-y-auto w-full max-w-2xl mx-auto lg:max-w-none no-scrollbar">
-          <div className="w-full max-w-[440px] relative z-10 perspective-[1000px]">
+        <div className="flex-1 flex flex-col items-center justify-start lg:justify-center p-6 sm:p-12 lg:p-16 h-[calc(100vh-64px)] overflow-y-auto w-full max-w-2xl mx-auto lg:max-w-none no-scrollbar py-12">
+          <div className="w-full max-w-[440px] relative z-10 perspective-[1000px] my-auto">
             {children}
           </div>
         </div>
